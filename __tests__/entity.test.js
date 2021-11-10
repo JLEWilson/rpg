9ef0da83e9476260af56d3fd7df23f950bbcff7b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-10 09:01:33 -0900

    Pass test for describe('Entity')
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- __tests__/entity.test.js
+++ __tests__/entity.test.js
@@ -1,16 +1,11 @@
 import Entity from "../src/entity";
 
 describe('Entity', () => {
-
   test('should create an object which stores 4 stats in a map: AP, DP, HP, XP', () => {
-    const entity = new Entity();
-    expect(entity.stats).toEqual(expect.anything());
-    expect(entity.stats[AP]).toEqual(expect.anything());
-    expect(entity.stats[DP]).toEqual(expect.anything());
-    expect(entity.stats[HP]).toEqual(expect.anything());
-    expect(entity.stats[XP]).toEqual(expect.anything());
-
+    const entity = new Entity(1, 2, 3, 4);
+    expect(entity.stats.get("AP")).toEqual(1);
+    expect(entity.stats.get("DP")).toEqual(2);
+    expect(entity.stats.get("HP")).toEqual(3);
+    expect(entity.stats.get("XP")).toEqual(4);
   });
-
-
 });
--- src/entity.js
+++ src/entity.js
@@ -1,5 +1,13 @@
-export default class Entity{
-  constructor(){
+export default class Entity {
+  constructor(att, def, health, exp) {
+    this.stats = new Map(
+      [
+        ["AP", att],
+        ["DP", def],
+        ["HP", health],
+        ["XP", exp]
+      ]
+    );
 
   }
 }
